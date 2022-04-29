d4bb72dfca529a8a3bd8ff7a3e4a9cfb88c1f10f
zayan6165 <mohammadtariqueraza786@gamil.com>
2022-04-29 16:52:10 +0530

    review

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Welcome');
+
+let obj=
+    {
+        name:"Hello",
+        profilePic:`/IMAGES/1.jpg`
+    }
+console.log(obj)
+ document.body.style.backgroundImage='url(obj.profilePice)'
+ 
